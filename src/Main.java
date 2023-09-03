e4bd0d96a4453a37734c9f100d0e6a77e3c1e494
GustavoLazoti <gustavo.lazotti@hotmail.com>
2023-09-03 15:03:02 -0300

    -> Scanner added to read data from user.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-
+import java.util.Scanner;
 public class Main
 {
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
         //Generating the first word to the stack
+        Scanner scanner = new Scanner(System.in);
         stack p = new stack ();
-        String frase = "A sacada da casa";
+        System.out.println("Digite a frase: ");
+        String frase = scanner.nextLine();
         for(int i = 0; i< frase.length(); i++) {
             p.push(frase.charAt(i));
         }
